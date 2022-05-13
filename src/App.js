25cd357caa94c0265ebb7ba5b0d911c2fb314220
EsotericZ <cjsand03@gmail.com>
2022-05-13 08:52:42 -0600

    its a box!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-function App() {
-  return (
-    <div className="App">
-      Darts
-    </div>
-  );
+import React, { Component } from 'react';
+import { Stage, Layer, Text } from 'react-konva';
+// import Konva from 'konva';
+
+import ColoredRect from './components/Test';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Darts
+//       <ColoredRect />
+//     </div>
+//   );
+// }
+class App extends Component {
+  render() {
+    // Stage is a div container
+    // Layer is actual canvas element (so you may have several canvases in the stage)
+    // And then we have canvas shapes inside the Layer
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try click on rect" />
+          <ColoredRect />
+        </Layer>
+      </Stage>
+    );
+  }
 }
 
 export default App;
